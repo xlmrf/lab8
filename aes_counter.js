8ea42f8ba61ed91f390c4e5f937cb0e344c90e5b
Hola <56066640+xlmrf@users.noreply.github.com>
2023-12-27 22:26:25 +0200

    Create aes_counter.js

---
+++ aes_counter.js
@@ -0,0 +1,43 @@
+function aes_ctr_encrypt(plaintext, key, nonce) {
+    function incCounter(counter) {
+        for (let i = counter.length - 1; i >= 0; i--) {
+            if (counter[i] === 255) {
+                counter[i] = 0;
+            } else {
+                counter[i]++;
+                break;
+            }
+        }
+    }
+
+    function blockXOR(block1, block2) {
+        const result = new Array(block1.length);
+        for (let i = 0; i < block1.length; i++) {
+            result[i] = block1[i] ^ block2[i];
+        }
+        return result;
+    }
+
+    const blockSize = 16;
+    const numBlocks = Math.ceil(plaintext.length / blockSize);
+    let counter = nonce.slice();
+    let ciphertext = '';
+
+    for (let i = 0; i < numBlocks; i++) {
+        const blockStart = i * blockSize;
+        const blockEnd = Math.min((i + 1) * blockSize, plaintext.length);
+        const block = plaintext.slice(blockStart, blockEnd);
+        const keyStream = aes_encrypt_block(counter, key);
+        const encryptedBlock = blockXOR(keyStream, block.split('').map(char => char.charCodeAt(0)));
+
+        ciphertext += String.fromCharCode.apply(null, encryptedBlock);
+        incCounter(counter);
+    }
+
+    return ciphertext;
+}
+
+function aes_encrypt_block(block, key) {
+    import("./aes_enc_block")
+    return block
+}
